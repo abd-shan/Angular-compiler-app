d8343edc6b78759e1ae5d19cdabb28cd949f01a7
Abdulkader-Shanbour <abd.alkader.shanbour@gmail.com>
2025-08-30 21:33:22 +0300

    add semantic error to import

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import ast.program.AngularApp;
 import semantic.TemplateSemanticAnalyzer;
 import semantic.TypeScriptSemanticAnalyzer;
+import semantic.ImportSemanticAnalyzer;
 import visitors.AngularVisitor;
 import java.io.IOException;
 import java.util.List;
@@ -26,50 +27,64 @@ public class Main {
 		AngularParser parser = new AngularParser(token);
 		ParseTree tree = parser.angularApp();
 
-		// build AST + symbol tables
+
+
 		visitor = new AngularVisitor(cs);
 		AngularApp program = (AngularApp) visitor.visit(tree);
 
-		// print ast.program (AST)
+
 		System.out.println(program);
 
-		// get symbol tables from visitor
+
 		var tsTable = visitor.getTsSymbolTable();
 		var templateTable = visitor.getTemplateSymbolTable();
 		var routerTable = visitor.getRouterSymbolTable();
 		var importTable = visitor.getImportSymbolTable();
 
-		// run semantic analyzer
+		// run semantic analyzers
 		TemplateSemanticAnalyzer tAnalyzer = new TemplateSemanticAnalyzer(tsTable, templateTable);
 		tAnalyzer.analyze();
-		TypeScriptSemanticAnalyzer  tsAnalyzer = new TypeScriptSemanticAnalyzer(tsTable);
+
+		TypeScriptSemanticAnalyzer tsAnalyzer = new TypeScriptSemanticAnalyzer(tsTable);
 		tsAnalyzer.analyze();
-		//merge errors
+
+		ImportSemanticAnalyzer importAnalyzer = new ImportSemanticAnalyzer(importTable);
+		importAnalyzer.analyze();
+
+		// merge errors from all analyzers
 		List<String> errors = new java.util.ArrayList<>(tsAnalyzer.getErrors());
 		errors.addAll(tAnalyzer.getErrors());
+		errors.addAll(importAnalyzer.getErrors());
+
 
 
 		System.out.println("\n\n\n======================== SEMANTIC CHECK ========================\n\n\n");
 		if (errors.isEmpty()) {
 			System.out.println("No semantic errors found ^_^");
+
+
+			System.out.println("\n\n\n<<<<<<<<<<<<<<<<<<<<<<<<< TS SYMBOL TABLE >>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n");
+			System.out.println(tsTable);
+
+			System.out.println("\n\n\n<<<<<<<<<<<<<<<<<<<<<<<<< TEMPLATE/BINDING SYMBOL TABLE >>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n");
+			System.out.println(templateTable);
+
+			System.out.println("\n\n\n<<<<<<<<<<<<<<<<<<<<<<<<< ROUTER SYMBOL TABLE >>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n");
+			System.out.println(routerTable);
+
+			importTable.printTable();
+
+
+			HtmlGenerator htmlGenerator = new HtmlGenerator(program, routerTable);
+
 		} else {
+
+
 			System.out.println("Semantic errors: 0_0");
 			for (String e : errors) System.out.println("- " + e);
+
+
+
 		}
-
-		// print symbol tables for debug
-		System.out.println("\n\n\n<<<<<<<<<<<<<<<<<<<<<<<<< TS SYMBOL TABLE >>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n");
-		System.out.println(tsTable);
-
-		System.out.println("\n\n\n<<<<<<<<<<<<<<<<<<<<<<<<< TEMPLATE/BINDING SYMBOL TABLE >>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n");
-		System.out.println(templateTable);
-
-		System.out.println("\n\n\n<<<<<<<<<<<<<<<<<<<<<<<<< ROUTER SYMBOL TABLE >>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n");
-		System.out.println(routerTable);
-		importTable.printTable();
-
-
-		HtmlGenerator htmlGenerator=new HtmlGenerator(program,routerTable);
-
 	}
 }
